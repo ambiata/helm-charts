f361d065224240efabfd877b57d0d31951907e66
Carlos Barboza <carlos.barboza@ambiata.com>
2021-03-22 17:25:22 +1100

    Make postgres-db-sync customisable, add support to Prometheus Push Gateway"

--- charts/postgres-db-sync/README.md
+++ charts/postgres-db-sync/README.md
@@ -1,4 +1,16 @@
-A chart to sync multuple postgres DBs using SQL Copy.
+A chart to sync multiple postgres tables using SQL Copy.
 
 We can provide a list of input -> output table mappings and a dictionary of database
-inputs and secret information and have the script sync each table periodically, based on a timestamp column.
+inputs and secret information and have the script sync each table periodically, based 
+on a timestamp column.
+
+Arguments to customise the job:
+
+- `customExportStatementConfigMap`: should point to a ConfigMap that contains a file named `export.sql` with the SQL code 
+to extract the records from the source database/tables.
+
+- `createTargetTableConfigMap`: should point to a Configmap that contains a file named `create_target.sql` with the SQL code
+to create the target table on the target database.
+  
+- `PrometheusPushGatewayURL`: URL of a Prometheus Push Gateway Server to send the metrics of the job.
+
--- charts/postgres-db-sync/table2table_sync.sh
+++ charts/postgres-db-sync/table2table_sync.sh
@@ -7,19 +7,26 @@ echo "to ${OUTPUT_DB_NAME}:${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\"";
 # Output DB Connection
 export PGHOST=${OUTPUT_DB_HOST} PGDATABASE=${OUTPUT_DB_NAME} PGUSER=${OUTPUT_DB_USER} PGPASSWORD=${OUTPUT_DB_PASS};
 
+# Create target table if required
+if [[ -f "/usr/src/pg-db-sync/create_target.sql" ]]
+then
+  echo "Creating ${OUTPUT_DB_NAME}:${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}";
+  psql --single-transaction -f /usr/src/pg-db-sync/create_target.sql;
+fi
+
 # Truncate table if required
 if [[ "$TRUNCATE_TARGET_TABLE" == "true" ]]
 then
   echo "Truncating ${OUTPUT_DB_NAME}:${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}";
-  psql  -c "TRUNCATE TABLE ${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\";";
+  psql -c "TRUNCATE TABLE ${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\";";
 fi
 
 # Find the oldest record in the output table so that we can export all the
 # newer records than this
-export OLDEST_RECORD=`(psql --tuples-only -c " \
+export OLDEST_RECORD=`(psql --tuples-only -c "\
   SELECT COALESCE(MAX(\"${TIMESTAMP_COLUMN}\"),'1900-01-01 00:00:00'::timestamp) \
   FROM ${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\"; \
-")`;
+" | xargs)`;
 
 echo "Oldest record timestamp at target ${OLDEST_RECORD}";
 
@@ -27,11 +34,11 @@ echo "Oldest record timestamp at target ${OLDEST_RECORD}";
 export PGHOST=${INPUT_DB_HOST} PGDATABASE=${INPUT_DB_NAME} PGUSER=${INPUT_DB_USER} PGPASSWORD=${INPUT_DB_PASS};
 
 # Copy all newer records from the input table -> CSV
-export EXPORTED_RECORDS=`(psql -c "\copy  \
-(SELECT * FROM ${INPUT_SCHEMA}.\"${INPUT_TABLE}\"  \
-WHERE \"${TIMESTAMP_COLUMN}\" > '${OLDEST_RECORD}'::timestamp  \
-	  AND \"${TIMESTAMP_COLUMN}\" < now()::timestamp - interval '${LAG_MINUTES}' minute)  \
-TO '/tmp/pg-to-pg-sync.copy' WITH CSV;" | sed 's/COPY //g')`;
+export EXPORTED_RECORDS=`(psql --single-transaction \
+  --set schema=${INPUT_SCHEMA} --set table=${INPUT_TABLE}  \
+  --set column=${TIMESTAMP_COLUMN} --set oldest_record="${OLDEST_RECORD}" \
+  --set lag_minutes=${LAG_MINUTES} -f /usr/src/pg-db-sync/export.sql | \
+  tail -n 1 | sed 's/COPY //g')`;
 
 echo "${EXPORTED_RECORDS} records exported from ${INPUT_SCHEMA}.\"${INPUT_TABLE}\"";
 
@@ -41,6 +48,20 @@ export PGHOST=${OUTPUT_DB_HOST} PGDATABASE=${OUTPUT_DB_NAME} PGUSER=${OUTPUT_DB_
 # Copy saved CSV -> Output Table
 export IMPORTED_RECORDS=`(psql -c "\copy ${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\" FROM '/tmp/pg-to-pg-sync.copy' WITH CSV;" | sed 's/COPY //g')`;
 
-echo "${EXPORTED_RECORDS} records exported from ${INPUT_DB_HOST}:${INPUT_SCHEMA}.\"${INPUT_TABLE}\"";
+echo "${IMPORTED_RECORDS} records imported to ${OUTPUT_DB_NAME}:${OUTPUT_SCHEMA}.\"${OUTPUT_TABLE}\"";
 
-# MAKE PROM METRICS
+# Prometheus Push Gateway Metrics
+if [[ -v PROMETHEUS_PUSHGATEWAY_URL ]]
+then
+  echo "Pushing Job Metrics to ${PROMETHEUS_PUSHGATEWAY_URL}";
+  METRICS_KEY="node=\"${K8S_NODE_NAME}\", namespace=\"${K8S_POD_NAMESPACE}\", pod_ip=\"${K8S_POD_IP}\"";
+  echo "Metrics Key is ${METRICS_KEY}";
+  cat <<EOF | curl --data-binary @- http://${PROMETHEUS_PUSHGATEWAY_URL}/metrics/job/${CRONJOB_NAME}/instance/${K8S_POD_NAME}
+# TYPE postgres_db_sync_imported_records counter
+postgres_db_sync_exported_records{${METRICS_KEY}} ${EXPORTED_RECORDS}
+# TYPE postgres_db_sync_imported_records counter
+postgres_db_sync_imported_records{${METRICS_KEY}} ${IMPORTED_RECORDS}
+EOF
+fi
+
+date;
